01a93ab3e398aa1258497c595ec91175e9f3f680
mdccg <matheusdanielgomess@gmail.com>
2021-05-26 15:30:37 -0400

    📅
    Data e militar adicionados

--- controle-de-viaturas-frontend/src/screens/Tabela/index.js
+++ controle-de-viaturas-frontend/src/screens/Tabela/index.js
@@ -3,16 +3,31 @@ import './styles.css';
 
 import api from './../../services/api';
 
+import moment from 'moment';
+
 function Tabela() {
+  const [data, setData] = useState('Carregando...');
+  const [ultimoMilitar, setUltimoMilitar] = useState('');
+
   const [viaturas, setViaturas] = useState([]);
 
   function buscarViaturas() {
-    api.get('/viaturas')
-      .then(async res => {
-        await setViaturas(res.data);
-        window.print();
+    api.get('/checkpoint')
+      .then(res => {
+        let { data, ultimoMilitar } = res.data;
+
+        setData(moment(data).format('DD.MM.YYYY'));
+        setUltimoMilitar(ultimoMilitar);
+
+        api.get('/viaturas')
+          .then(async res => {
+            await setViaturas(res.data);
+            window.print();
+          })
+          .catch(err => console.error(err));
       })
       .catch(err => console.error(err));
+
   }
 
   useEffect(() => {
@@ -21,6 +36,7 @@ function Tabela() {
 
   return (
     <div className="tabela">
+      <h1>{data} - {ultimoMilitar}</h1>
       <table>
         <tr>
           <th>Prefixo</th>
