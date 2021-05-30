e5756fabc05b683050abac0c6154087f336ad301
mdccg <matheusdanielgomess@gmail.com>
2021-05-29 21:36:29 -0400

    🏹
    URL da API simulada adicionada

--- controle-de-viaturas-frontend/src/services/api.js
+++ controle-de-viaturas-frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://controle-de-viaturas-api.herokuapp.com/api/v1'
-  baseURL: 'http://192.168.2.8:3001/api/v1',
+  baseURL: 'https://controle-de-viaturas-mock-api.herokuapp.com/api/v1'
 });
 
 export default api;
